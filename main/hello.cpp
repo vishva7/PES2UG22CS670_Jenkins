03d864319fece1184a49bd4c0d0a5c9d3b7367d8
vishva7 <vishvamehta301@gmail.com>
2025-03-11 15:43:54 +0530

    changed hello.cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "This is my SRN: PES2UG22CS670\n";
   return 0;
 }
